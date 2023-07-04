e789fabcdec3b503a61cc0188c2b4350bd8338a6
samuel <samuelkelvinmwangi@gmail.com>
2023-07-04 11:33:59 +0300

    assingnment

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,14 @@
-
-
+class Book
+    attr_accessor :title, :author_name, :page_count, :genre
+  
+    def initialize(title)
+      @title = title
+      @author_name = author_name
+      @page_count = page_count
+      @genre = genre
+    end
+  
+    def turn_page
+      puts "Flipping the page...wow, you read fast!"
+    end
+  end
